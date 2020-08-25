7b91e8bd8f5c8c87ea7fd5e5289313d6eb7cc6ae
Puheary <tjd6976@naver.com>
2020-08-25 16:22:46 +0900

    first commit

---
+++ public/precache-manifest.4ff45759c02603ff6a34149daf5ce7f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4296638824b21ef648",
+    "url": "/css/app.2bcefe64.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ecdc8695878f00be8fa54c3007029c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a4296638824b21ef648",
+    "url": "/js/app.9a27dcc4.js"
+  },
+  {
+    "revision": "75004f9328a4742b922c",
+    "url": "/js/chunk-vendors.abfe0df9.js"
+  },
+  {
+    "revision": "a7bb829f05709f04c4cc8f0bb0289d89",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
